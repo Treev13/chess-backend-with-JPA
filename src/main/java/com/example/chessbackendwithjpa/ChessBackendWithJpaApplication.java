11b359a0b98a4037052e1f8e5540ca3e826abf16
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-08-05 15:03:18 +0200

    start a Maven Project

---
+++ src/main/java/com/example/chessbackendwithjpa/ChessBackendWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chessbackendwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBackendWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBackendWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chessbackendwithjpa/ChessBackendWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessbackendwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBackendWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
